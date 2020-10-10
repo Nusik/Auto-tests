062d697fb7c950fa006d9db5349a680862e94303
Inna Hupalo <inna@y-tree.com>
2020-10-10 21:52:28 +0300

    Update GoogleSearchResultInListTest

--- src/test/java/GoogleSearchResultInListTest.java
+++ src/test/java/GoogleSearchResultInListTest.java
@@ -1,6 +1,7 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -23,28 +24,24 @@ public class GoogleSearchResultInListTest extends BaseUiTest {
 
     @Test
     public void searchLinkInResultTest() {
-        String siteName = "stylus.ua";
+        int count = 1;
 
         driver.findElement(By.name("q")).sendKeys("iphone kyiv buy" + Keys.ENTER);
-        List<WebElement> links = driver.findElements(By.cssSelector("#cite"));        // не нашел сайт стилуса, хотя в результате на 1ой странице
-//        for (WebElement element : links) {
-//           System.out.println("site: " + element.getText());}
 
-        for (int i = 1; i <= 5; i = i + 1) {
-            if (!links.contains(siteName)) {
+        WebElement link = driver.findElement(By.partialLinkText("stylus.ua"));
 
+        if (link.isDisplayed()) {
+            System.out.println("STYLUS.UA found on " + count + " page");
+        } else {
+            while (count <= 5) {
                 driver.findElement(By.cssSelector("#pnnext")).click();
-                List<WebElement> link2 = driver.findElements(By.cssSelector("#cite"));
-
-                if (!link2.contains(siteName)) {
-                    return;
+                driver.findElement(By.partialLinkText("stylus.ua"));
+                count++;
+                if (link.isDisplayed()) {
+                    System.out.println("STYLUS.UA found on " + count + " page");
                 } else {
-                    System.out.println(siteName.toUpperCase() + " found on page " + i);
+                    System.out.println("STYLUS.UA not found on first 5 pages");
                 }
-                System.out.println(siteName.toUpperCase() + " not found on page " + i);  //как указать номер страницы? какой цикл нужен, чтоб искать на каждой странице?
-
-            } else {
-                System.out.println(siteName.toUpperCase() + " found on page " + i);
             }
         }
     }
