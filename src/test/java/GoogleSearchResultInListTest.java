aeb45e8fa94e82e030abd2124eb44cda93117cac
Inna Hupalo <inna@y-tree.com>
2020-10-10 22:19:34 +0300

    Update GoogleSearchResultInListTest

--- src/test/java/GoogleSearchResultInListTest.java
+++ src/test/java/GoogleSearchResultInListTest.java
@@ -1,17 +1,8 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
-import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
-import javax.sound.midi.Soundbank;
-import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.Objects;
-
-import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
-import static org.testng.Assert.assertTrue;
 
 public class GoogleSearchResultInListTest extends BaseUiTest {
 
